7a1e2b8d493a18bdacad7016f15f20f872091f34
jackieonway <1254825871@qq.com>
2020-05-19 20:00:54 +0800

    feat: import activiti process definition file
    import activiti process definition file

--- src/main/java/com/github/jackieonway/activiti/controller/activiti/activiti/ActivitiModelController.java
+++ src/main/java/com/github/jackieonway/activiti/controller/activiti/activiti/ActivitiModelController.java
@@ -7,10 +7,12 @@ import com.github.jackieonway.activiti.utils.ResponseUtils;
 import com.github.jackieonway.activiti.utils.ResultMsg;
 import com.github.jackieonway.activiti.utils.page.PageResult;
 import com.github.jackieonway.activiti.utils.page.QueryConditionBean;
+import com.sun.xml.internal.fastinfoset.stax.factory.StAXInputFactory;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.editor.constants.ModelDataJsonConstants;
@@ -18,19 +20,22 @@ import org.activiti.editor.language.json.converter.BpmnJsonConverter;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.Model;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+@Slf4j
 @CrossOrigin
 @RestController
 @Api(value = "工作流模型相关接口", tags = "工作流模型相关接口")
@@ -86,7 +91,7 @@ public class ActivitiModelController {
                 PageResult.newEmptyResult(queryConditionBean.getPageNum(), queryConditionBean.getPageSize());
         List<Model> models = repositoryService.createModelQuery().listPage(queryConditionBean.getStartIndex(),
                 queryConditionBean.getPageSize());
-        if (CollectionUtils.isEmpty(models)){
+        if (CollectionUtils.isEmpty(models)) {
             return pageResult;
         }
         pageResult.setTotalCount(repositoryService.createModelQuery().count());
@@ -100,7 +105,8 @@ public class ActivitiModelController {
      */
     @ApiOperation(value = "根据模型id发布模型为流程定义", notes = "必传参数: modelId: 模型id")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "modelId", paramType = "query", value = "模型id", required = true, dataType = "String")})
+            @ApiImplicitParam(name = "modelId", paramType = "query", value = "模型id", required = true, dataType =
+                    "String")})
     @GetMapping("/deploy")
     @ResponseBody
     public ResultMsg deploy(String modelId) throws IOException {
@@ -128,4 +134,60 @@ public class ActivitiModelController {
         repositoryService.saveModel(modelData);
         return ResponseUtils.success();
     }
+
+    @ApiOperation(value = "导入流程文件", notes = "必传参数: file: 流程定义文件 支持： bpmn20.xml 和 bpmn文件格式")
+    @PostMapping(value = "/importBpmn")
+    public void importBpmn(@RequestParam("file") MultipartFile uploadfile) {
+        InputStreamReader in = null;
+        try {
+            String fileName = uploadfile.getOriginalFilename();
+            if (!(".bpmn20.xml".endsWith(fileName) || ".bpmn".endsWith(fileName))) {
+                log.error("activiti import error , file name is [{}], but is not \".bpmn20.xml\" or \".bpmn\" : [{}]"
+                        , fileName);
+                return;
+            }
+            XMLInputFactory xif = new StAXInputFactory();
+            in = new InputStreamReader(new ByteArrayInputStream(uploadfile.getBytes()), StandardCharsets.UTF_8);
+            XMLStreamReader xtr = xif.createXMLStreamReader(in);
+            BpmnModel bpmnModel = new BpmnXMLConverter().convertToBpmnModel(xtr);
+
+            if (bpmnModel.getMainProcess() == null || bpmnModel.getMainProcess().getId() == null) {
+                log.error("activiti import error ,activiti can not find main process");
+                return;
+            }
+            if (bpmnModel.getLocationMap().isEmpty()) {
+                log.error("activiti import error ,activiti can not find location map");
+                return;
+            }
+            String processName;
+            if (StringUtils.isNotEmpty(bpmnModel.getMainProcess().getName())) {
+                processName = bpmnModel.getMainProcess().getName();
+            } else {
+                processName = bpmnModel.getMainProcess().getId();
+            }
+            Model modelData;
+            modelData = repositoryService.newModel();
+            ObjectNode modelObjectNode = new ObjectMapper().createObjectNode();
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, processName);
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+            modelData.setMetaInfo(modelObjectNode.toString());
+            modelData.setName(processName);
+            repositoryService.saveModel(modelData);
+            BpmnJsonConverter jsonConverter = new BpmnJsonConverter();
+            ObjectNode editorNode = jsonConverter.convertToJson(bpmnModel);
+            repositoryService.addModelEditorSource(modelData.getId(),
+                    editorNode.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            log.error("activiti import error , reason : [{}]", e.getMessage(), e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    log.error("activiti import error , reason : [{}]", e.getMessage(), e);
+                }
+            }
+        }
+    }
+
 }
